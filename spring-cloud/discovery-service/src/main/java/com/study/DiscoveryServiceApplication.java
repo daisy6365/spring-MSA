9aaaa81664856db60142ddafee8bac5681910a22
한다빈 <gksekqls9808@naver.com>
2023-02-08 22:07:03 +0900

    feat: Service Discovery(user-service)의 Load Balancer

--- spring-cloud/discovery-service/src/main/java/com/study/DiscoveryServiceApplication.java
+++ spring-cloud/discovery-service/src/main/java/com/study/DiscoveryServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 // 주소를 저장해 놓는 곳 (주소검색 시스템)
 @SpringBootApplication
-@EnableEurekaServer //EurekaServer로 동작할 수 있음
+@EnableEurekaServer // EurekaServer로 동작할 수 있음
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
