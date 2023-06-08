587f0280345574e7192998d1172b277d6dd9cdd2
한다빈 <gksekqls9808@naver.com>
2023-06-08 17:52:51 +0900

    feat: Users Microservice - Routes 정보 변경

--- microservices/user-service/src/main/java/com/study/security/WebSecurity.java
+++ microservices/user-service/src/main/java/com/study/security/WebSecurity.java
@@ -1,26 +1,70 @@
 package com.study.security;
 
 
+import com.study.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
+
+    private final Environment env;
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ObjectPostProcessor<Object> objectPostProcessor;
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/",
+            "/**"
+    };
 
     // WebSecurityConfigurerAdapter, authorizeRequest() Deprecated됨
     // Bean에 등록하여 사용
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().requestMatchers("/user-service/**").permitAll();
-        http.authorizeHttpRequests().requestMatchers("/user-service/**").permitAll();
+        http.authorizeHttpRequests(authorize -> {
+                    try {
+                        authorize
+                                .requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll()
+                                .and()
+                                .addFilter(getAuthenticationFilter());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
         http.headers().frameOptions().disable();
 
         return http.build();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+
+        return authenticationFilter;
     }
 
 }
--- microservices/user-service/src/main/java/com/study/service/UserService.java
+++ microservices/user-service/src/main/java/com/study/service/UserService.java
@@ -2,8 +2,9 @@ package com.study.service;
 
 import com.study.domain.UserEntity;
 import com.study.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     UserDto createUser(UserDto userDto);
 
