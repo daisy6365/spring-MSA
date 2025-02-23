51c70f78aa5cd83a92dfdbfa22d439893fa27ab2
han dabin <gksekqls9808@naver.com>
2025-02-23 23:29:52 +0900

    update: 2025 version.

--- spring-cloud/discovery-service/src/main/java/com/study/DiscoveryServiceApplication.java
+++ spring-cloud/discovery-service/src/main/java/com/study/DiscoveryServiceApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 // 주소를 저장해 놓는 곳 (주소검색 시스템)
 @SpringBootApplication
-@EnableEurekaServer // EurekaServer로 동작할 수 있음
+// EurekaServer로 동작할 수 있음
+// Auto-Configuration을 통해 EurekaServer 관련 bean 등록
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
