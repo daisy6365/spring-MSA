ca1c7ffa1fc8797ee44454451141277ab574affa
daisy6365 <gksekqls9808@naver.com>
2025-02-24 23:36:20 +0900

    feat: GlobalFilter

--- spring-cloud/gateway-service/src/main/java/com/study/filter/GlobalFilter.java
+++ spring-cloud/gateway-service/src/main/java/com/study/filter/GlobalFilter.java
@@ -9,6 +9,10 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+/**
+ * 어떤 요청 EndPoint가 와도, 공통적으로 수행되는 필터
+ * application.yml에도 한번에 설정 가능
+ */
 @Slf4j
 @Component
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
@@ -27,6 +31,7 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 
             log.info("Global filter baseMessage: {}", config.getBaseMessage());
 
+            // @Getter를 통해서 가져온 것임
             if (config.isPreLogger()) {
                 log.info("Global filter Start: request id -> {}", request.getId());
             }
@@ -39,6 +44,13 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
         });
     }
 
+    /**
+     * 필요한 매개변수의 값는 application.yml파일에 설정 제어 가능
+     * @Setter @Getter를 위해 @Data 해놓음
+     *
+     * 단점 : 설정정보가 APPlication.yml에 존재 한다?
+     * 수정 시 값을 변경하고 다시 빌드하는 번거로움을 진행해야 함
+     */
     @Data
     public static class Config{
         private String baseMessage;
