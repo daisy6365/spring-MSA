79e144fd20879f6c0953a26c1e8d94e049b22a35
daisy6365 <gksekqls9808@naver.com>
2025-03-05 23:46:26 +0900

    config: setting config server

--- spring-cloud/config-service/src/main/java/com/study/configservice/ConfigServiceApplication.java
+++ spring-cloud/config-service/src/main/java/com/study/configservice/ConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.study.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// Auto-Configuration 으로 동작
+@EnableConfigServer // Config server로 등록
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
