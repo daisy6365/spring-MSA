5bae35e113b143f89ef98103020bc0ab53040397
daisy6365 <gksekqls9808@naver.com>
2025-03-17 00:02:07 +0900

    comment: kafka consumer - 해당 토픽을 통해 데이터 감지

--- microservices/catalog-service/src/main/java/com/study/kafka/KafkaConsumer.java
+++ microservices/catalog-service/src/main/java/com/study/kafka/KafkaConsumer.java
@@ -20,13 +20,17 @@ public class KafkaConsumer {
 
     private final CatalogRepository catalogRepository;
 
+    /**
+     * 상품 업데이트 토픽에 따라 데이터 반영
+     */
     @KafkaListener(topics = "example-catalog-topic")
     public void updateQty(String kafkaMessage){
         log.info("Kafka Message: ->" + kafkaMessage);
 
         Map<Object, Object> map = new HashMap<>();
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper(); // 데이터 매핑
         try {
+            // 들어온 String 타입의 kafkaMessage를 파싱함
             map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
         }catch (JsonProcessingException ex){
             ex.printStackTrace();
@@ -34,6 +38,7 @@ public class KafkaConsumer {
 
         CatalogEntity entity = catalogRepository.findByProductId((String) map.get("productId"));
         if(entity != null){
+            // 수량 업데이트
             entity.setStock(entity.getStock() - (Integer) map.get("qty"));
             catalogRepository.save(entity);
         }
